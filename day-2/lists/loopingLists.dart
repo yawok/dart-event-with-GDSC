5d1d93e6b2db6e5704f27bd49a21cc73e6407b42
yaw.o.k <obengky72@gmail.com>
2022-12-07 14:34:42 +0000

    Add demo on list looping and user input collection

---
+++ day-2/lists/loopingLists.dart
@@ -0,0 +1,23 @@
+// author: Kenneth Yaw Obeng(yawok)
+// demo on looping through lists
+
+void main(List<String> args) {
+  List<String> names = ['Ruth', 'josh', 'fred'];
+  printList(names);
+
+  print('\nSimplified looping ::');
+  printListSimplified(names);
+}
+
+
+void printList(List list) {
+  for(int i = 0; i < list.length; i++){
+    print(list[i]);
+  }
+}
+
+void printListSimplified(List list) {
+  for(final element in list){
+    print(element);
+  }
+}
