139a754a1a5fc315b53b6f8c87f8ee1147acca02
yaw.o.k <obengky72@gmail.com>
2022-12-07 08:22:37 +0000

    Make function call

--- day-2/functionsWithPositionalParams.dart
+++ day-2/functionsWithPositionalParams.dart
@@ -2,7 +2,11 @@
 // demo of a function with positional parameters
 
 void main(List<String> args) {
-  
+  var authorName1 = fullName("Stephen", "King");
+  var authorName2 = fullName("Larry", "Page", "Kwadwo");
+
+  print(authorName1);
+  print(authorName2);
 }
 
 String fullName(String firstName, String lastName, [String? middleName]) {
